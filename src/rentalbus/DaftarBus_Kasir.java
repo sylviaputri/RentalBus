204cf0931d807cad90536bad3c23b91b30bc0dcc
sylviaputri <sylvi.bluemochi@gmail.com>
2018-11-27 02:37:27 +0700

    DaftarBus -> searching + sorting

--- src/rentalbus/DaftarBus_Kasir.java
+++ src/rentalbus/DaftarBus_Kasir.java
@@ -27,12 +27,15 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
     DetailBus_Kasir detailBus;
     ArrayList<Bus> busList;
     
+    //CONSTRUCTOR
     public DaftarBus_Kasir(String nama) throws SQLException {
         initComponents();
         this.nama = nama;
-        show_bus();
+        ArrayList<Bus> list = busList();
+        show_bus(list);
     }
     
+    //Mendapatkan semua data BUS dari DB (tanpa searching)
     public ArrayList<Bus> busList() throws SQLException{
         busList = new ArrayList<>();
         ArrayList<JenisBus> jenisBusList = jenisBusList();
@@ -61,6 +64,7 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
         return busList;
     }
     
+    //Mendapatkan semua data JENIS BUS dari DB (tanpa searching)
     public ArrayList<JenisBus> jenisBusList() throws SQLException{
         ArrayList<JenisBus> jenisBusList = new ArrayList<>();
         try{
@@ -81,19 +85,6 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
         return jenisBusList;
     }
     
-    public void show_bus() throws SQLException{
-        ArrayList<Bus> list = busList();
-        DefaultTableModel model = (DefaultTableModel)tableBus.getModel();
-        Object[] row = new Object[4];
-        for(int i=0;i<list.size();i++){
-            row[0] = list.get(i).getNopol();
-            row[1] = list.get(i).getJenisBus().getNamaJenis();
-            row[2] = list.get(i).getJenisBus().getHargaSewa();
-            row[3] = list.get(i).getStatusSewa();
-            model.addRow(row);
-        }
-    }
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -103,15 +94,15 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tableBus = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        txtSearch = new javax.swing.JTextField();
+        btnSearch = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        comboSearch = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        comboSorting = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
-        jComboBox3 = new javax.swing.JComboBox<>();
-        jTextField2 = new javax.swing.JTextField();
+        comboAdvancedSearch = new javax.swing.JComboBox<>();
+        txtAdvancedSearch = new javax.swing.JTextField();
         btnBack = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -145,19 +136,24 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
 
         jLabel1.setText("Daftar Bus");
 
-        jButton1.setText("Search");
+        btnSearch.setText("Search");
+        btnSearch.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnSearchMouseClicked(evt);
+            }
+        });
 
         jLabel2.setText("Search by");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Jenis", "Status" }));
+        comboSearch.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Jenis", "Status" }));
 
         jLabel3.setText("Sorting by");
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Harga" }));
+        comboSorting.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Harga" }));
 
         jLabel4.setText("Advanced search by");
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Jenis", "Status" }));
+        comboAdvancedSearch.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nopol", "Jenis", "Status" }));
 
         btnBack.setText("Back");
         btnBack.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -180,7 +176,7 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(23, 23, 23)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jButton1)
+                            .addComponent(btnSearch)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -189,16 +185,16 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
                                             .addComponent(jLabel3))
                                         .addGap(39, 39, 39)
                                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                            .addComponent(jComboBox3, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                            .addComponent(comboAdvancedSearch, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(comboSorting, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                     .addGroup(jPanel1Layout.createSequentialGroup()
                                         .addComponent(jLabel2)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addComponent(comboSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(jTextField1)
-                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(txtSearch)
+                                    .addComponent(txtAdvancedSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 843, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(54, Short.MAX_VALUE))
         );
@@ -211,20 +207,20 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
                     .addComponent(btnBack))
                 .addGap(38, 38, 38)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(comboSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtAdvancedSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(comboAdvancedSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(comboSorting, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
+                .addComponent(btnSearch)
                 .addGap(28, 28, 28)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(26, Short.MAX_VALUE))
@@ -272,11 +268,92 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
         detailBus.lblStatus.setText(bus.getStatusSewa());
     }//GEN-LAST:event_tableBusMouseClicked
 
+    //balik ke halaman HOME PAGE KASIR
     private void btnBackMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnBackMouseClicked
         this.setVisible(false);
         new HomePage_Kasir(nama).setVisible(true);
     }//GEN-LAST:event_btnBackMouseClicked
 
+    //kalau button SEARCH diklik
+    private void btnSearchMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSearchMouseClicked
+        DefaultTableModel model = (DefaultTableModel)tableBus.getModel();
+        int tableSize = model.getRowCount();
+        for (int i = tableSize - 1; i >= 0; i--) {
+            model.removeRow(i);
+        }
+        ArrayList<Bus> list = null;
+        try {
+            list = busListWithSearch();
+        } catch (SQLException ex) {
+            Logger.getLogger(DaftarBus_Kasir.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        show_bus(list);
+    }//GEN-LAST:event_btnSearchMouseClicked
+
+    //Menampilkan data BUS kedalam TABEL
+    public void show_bus(ArrayList<Bus> list){
+        DefaultTableModel model = (DefaultTableModel)tableBus.getModel();
+        Object[] row = new Object[4];
+        for(int i=0;i<list.size();i++){
+            row[0] = list.get(i).getNopol();
+            row[1] = list.get(i).getJenisBus().getNamaJenis();
+            row[2] = list.get(i).getJenisBus().getHargaSewa();
+            row[3] = list.get(i).getStatusSewa();
+            model.addRow(row);
+        }
+    }
+    
+    //Mendapatkan data BUS dari DB menggunakan SEARCHING
+    public ArrayList<Bus> busListWithSearch() throws SQLException{
+        busList = new ArrayList<>();
+        ArrayList<JenisBus> jenisBusList = jenisBusList();
+        try{
+            kon.connect();
+            String searchBy="";
+            if(comboSearch.getSelectedItem().toString().equals("Nopol"))
+                searchBy="nopol_bus";
+            else if(comboSearch.getSelectedItem().toString().equals("Jenis"))
+                searchBy="nama_jenis";
+            else if(comboSearch.getSelectedItem().toString().equals("Status"))
+                searchBy="status_sewa";
+            
+            String advanchedSearchBy="";
+            if(comboAdvancedSearch.getSelectedItem().toString().equals("Nopol"))
+                advanchedSearchBy="nopol_bus";
+            else if(comboAdvancedSearch.getSelectedItem().toString().equals("Jenis"))
+                advanchedSearchBy="nama_jenis";
+            else if(comboAdvancedSearch.getSelectedItem().toString().equals("Status"))
+                advanchedSearchBy="status_sewa";
+            
+            String sortingBy="";
+            if(comboSorting.getSelectedItem().toString().equals("Nopol"))
+                sortingBy="nopol_bus";
+            else if(comboSorting.getSelectedItem().toString().equals("Harga"))
+                sortingBy="harga_sewa";
+            
+            kon.createQuery("select * from bus, jenis_bus where bus.jenis_bus = jenis_bus.id_jenis and "+searchBy+" like '%"+txtSearch.getText().toString()+"%' and "+advanchedSearchBy+" like '%"+txtAdvancedSearch.getText().toString()+"%' order by "+sortingBy);
+            Bus bus;
+            JenisBus jenisBus = null;
+            while(kon.myRs.next()){
+                for(int i=0;i<jenisBusList.size();i++){
+                    if(kon.myRs.getString("jenis_bus").equals(jenisBusList.get(i).getIdJenis())){
+                        jenisBus=jenisBusList.get(i);
+                        break;
+                    }
+                }
+                bus = new Bus(kon.myRs.getString("nopol_bus"), kon.myRs.getString("nama_sopir"), jenisBus, kon.myRs.getString("status_sewa"));
+                busList.add(bus);
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        finally{
+            kon.close();
+        }
+        return busList;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -294,10 +371,10 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBack;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JComboBox<String> jComboBox3;
+    private javax.swing.JButton btnSearch;
+    private javax.swing.JComboBox<String> comboAdvancedSearch;
+    private javax.swing.JComboBox<String> comboSearch;
+    private javax.swing.JComboBox<String> comboSorting;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -305,8 +382,8 @@ public class DaftarBus_Kasir extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JTable tableBus;
+    private javax.swing.JTextField txtAdvancedSearch;
+    private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
